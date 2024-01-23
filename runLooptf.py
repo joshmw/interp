9ab36a3dd597ab7cc5a50348d5321f3dc74e9cc5
Tejas Narayanan <tejasn100@gmail.com>
2024-01-23 09:56:57 -0800

    Update runLooptf

--- runLooptf.py
+++ runLooptf.py
@@ -1,79 +1,13 @@
 import subprocess
 print('0118')
 
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/grass.jpg", "-j", "in/leaves.jpg"]
-    subprocess.run(command)
+interps = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+image_pairs = [('in/grass.jpg', 'in/leaves.jpg'),
+               ('in/pebbles2.jpg', 'in/granite.jpg'),
+               ('in/b1.jpg', 'in/b6.jpg')]
 
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/pebbles2.jpg", "-j", "in/granite.jpg"]
-    subprocess.run(command)    
-    
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/b1.jpg", "-j", "in/b6.jpg"]
-    subprocess.run(command)
+for image1, image2 in image_pairs:
+    for interp in interps:
+        command = ["python3", "synthesize.py", "-t", str(interp), "-k", "activation", "-i", image1, "-j", image2]
+        subprocess.run(command)
 
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/acorns.jpg", "-j", "in/redwood.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/canopy.jpg", "-j", "in/leaves.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/canopy.jpg", "-j", "in/moss.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/fern.jpg", "-j", "in/grass.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/fern.jpg", "-j", "in/leaves.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/lemons.jpg", "-j", "in/bananas.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/mango.jpg", "-j", "in/yellowGems.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/obsidian.jpg", "-j", "in/licorice.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/ocean.jpg", "-j", "in/sky.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/pine.jpg", "-j", "in/grass.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/pine.jpg", "-j", "in/moss.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/redwood.jpg", "-j", "in/rockwall.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/rubies.jpg", "-j", "in/cherries.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/snow.jpg", "-j", "in/pebbles2.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/moss.jpg", "-j", "in/grass.jpg"]
-    subprocess.run(command)
-
-for interp in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    command = ["python3", "synthesize.py", "-t", str(interp), "-p", str(25), "-i", "in/moss.jpg", "-j", "in/leaves.jpg"]
-    subprocess.run(command)
