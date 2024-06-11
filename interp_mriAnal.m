db2b730c3b96b4d16d44adedc38a24eca8e21bd0
Josh Wilson <joshmw@stanford.edu>
2024-06-11 16:41:19 -0700

    fix repeated stimuli in mlds. add mlds for individual ROIs. add option to downsample number of trails

--- interp_mriAnal.m
+++ interp_mriAnal.m
@@ -7,11 +7,27 @@ function interp_mriAnal(varargin)
 %  Authors: Josh wilson
 %  Date: 04/16/2024
 %
+%  Arguments:
+%    reliability_cutoff: split-half correlation of betas used to select voxels for analysis
+%    r2cutoff: r2 cutoff (from glmsingle) used to select voxels for analysis
+%    stdCutoff: remove voxels whos betas in any single condition have a standard deviation higher than this value
+%    shuffleData: set to 1 to randomize condition labels in the very beginning (control measure)
+%    zscorebetas: set to 1 to zscore the betas in each run
+%    numBoots: number of bootstraps for split-half correlation analysis
+%    showAvgMDS/showDistancesSingleTrial: flags to show analyses I don't think are relevant
+%    mldsReps: number of mlds bootstraps. if set above 1, when averaging each condition, will average a random subset of trials instead of all
+%    plotBig: plots things in their own graphs rather than as subplots
+%    numBetasEachScan: Used for zscoring. Should be the number of betas you get from each scan from glmSingle (e.g. 12 conds x 4 repeats = 48)
+%    numScansInGLM: Used for zscoring. Needed to zscore over individual scans.
+%    numStimRepeats: Number of repeats of each stimulus (how many times shown in scanner)
+%    truncateTrials: If you want to useless data. Should set as a fraction of m/n, where n is numScansInGLM and m is the number of scans you want to use for data.
+
 
 
 %% Load the data
 %get args
-getArgs(varargin, {'reliability_cutoff=.65', 'r2cutoff=0', 'stdCutoff=5', 'shuffleData=0', 'zscorebetas=0', 'numBoots=100', 'showAvgMDS=0', 'showDistancesSingleTrials=0'});
+getArgs(varargin, {'reliability_cutoff=.65', 'r2cutoff=0', 'stdCutoff=5', 'shuffleData=0', 'zscorebetas=1', 'numBoots=100', 'showAvgMDS=50', 'showDistancesSingleTrials=0', 'mldsReps=1', 'plotBig=0',...
+    'numBetasEachScan=48', 'numScansInGLM=10', 'numStimRepeats=40','truncateTrials=(10/10)'});
 
 % Task 1 is right visual field  so LEFT HEMISPHERE roi's should be responsive
 % Task 2 is LEFT visual field, so RIGHT HEMISPHERE roi's shold be responsive
@@ -39,12 +55,25 @@ end
 
 %rename rois to contra and ipso so we can combine
 roiNames = task{1}.roiNames;
-for roi = 1:length(roiNames),
-    if roiNames{roi}(1) == 'l';
+for roi = 1:length(roiNames)
+    if roiNames{roi}(1) == 'l'
         roiNames{roi} = strcat('contra ', roiNames{roi}(2:end));
     else
         roiNames{roi} = strcat('ipso ', roiNames{roi}(2:end));
     end
+end
+
+
+%% if you want to truncate the data (fewer repeats), do so here by setting truncateTrials <1
+%note that we calculated reliability before truncating. Might want to switch.
+numStimRepeats = numStimRepeats * truncateTrials;
+numScansInGLM = numScansInGLM * truncateTrials;
+for taskNum = 1:2,
+    task{taskNum}.models.FIT_HRF_GLMdenoise_RR.modelmd = task{taskNum}.models.FIT_HRF_GLMdenoise_RR.modelmd(:,:,:,1:(numBetasEachScan*numScansInGLM));
+    task{taskNum}.trial_conditions = task{taskNum}.trial_conditions(1:(numBetasEachScan*numScansInGLM));
+    %you can make it backwards if you uncomment these. the proportion included will flip (.7 truncate -> include last .3 of data). DO NOT UNCOMMENT if not truncating.
+    %task{taskNum}.models.FIT_HRF_GLMdenoise_RR.modelmd = task{taskNum}.models.FIT_HRF_GLMdenoise_RR.modelmd(:,:,:,((numBetasEachScan*numScansInGLM)+1):end);
+    %task{taskNum}.trial_conditions = task{taskNum}.trial_conditions((numBetasEachScan*numScansInGLM+1):end);
 end
 
 
@@ -70,7 +99,6 @@ for taskNum = 1:2;
         
         s1a = cat(2, s1a{:});
         s2a = cat(2, s2a{:});
-    
         reliability(:,boot) = diag(corr(s1a', s2a'));
     
     end
@@ -78,19 +106,20 @@ for taskNum = 1:2;
     task{taskNum}.reliability = mean(reliability,2);
 end
 
-
-
 %% Average together the same stimulus presentations, partition by ROI and get rid of voxels under reliability cutoff
 %average together trials that were of the same stim type
 for taskNum = 1:2
     %get betas
     amplitudes = squeeze(task{taskNum}.models.FIT_HRF_GLMdenoise_RR.modelmd);
+
     %zscore if you want
     if zscorebetas
-        for scan = 1:10
-            amplitudes(:,(1 + ((scan-1)*40)):(scan*40)) = zscore(amplitudes(:,(1 + ((scan-1)*40)):(scan*40)), 0, 2);
+        for scan = 1:numScansInGLM
+            %zscore by scan
+            amplitudes(:,(1 + ((scan-1)*numBetasEachScan)):(scan*numBetasEachScan)) = zscore(amplitudes(:,(1 + ((scan-1)*numBetasEachScan)):(scan*numBetasEachScan)), 0, 2);
         end
     end
+
     %average
     for stim = 1:length(task{taskNum}.stimNames)
         task{taskNum}.averaged_amplitudes(:,stim) = nanmean(amplitudes(:,(task{taskNum}.trial_conditions == stim)),2);
@@ -115,7 +144,8 @@ end
 %% plot split half reliability, R2, and beta amplitudes by ROI
 
 %plot all the reliabilities with the average
-figure, subplot(3,1,1), hold on
+figure, 
+if ~plotBig, subplot(3,1,1), hold on, else, figure, hold on, end
 for roi = 1:length(roiNames)
     %add the two halves
     reliability = [task{1}.reliability(task{1}.whichROI == roi); task{2}.reliability(task{2}.whichROI == roi)];
@@ -133,7 +163,7 @@ ylim([-.5 .5])
 title('Correlation between first half and second half Betas')
 
 %plot the R2 with the average
-subplot(3,1,2), hold on
+if ~plotBig, subplot(3,1,2), hold on, else, figure, hold on, end
 for roi = 1:length(roiNames)
     r2 = [task{1}.models.FIT_HRF_GLMdenoise_RR.R2(task{1}.whichROI == roi); task{2}.models.FIT_HRF_GLMdenoise_RR.R2(task{2}.whichROI == roi)];
     scatter(repmat(roi,length(r2),1), r2,'k','filled','MarkerFaceAlpha',.05)
@@ -148,7 +178,7 @@ hline(0,':k')
 title('R-Squared of voxels')
 
 %plot the betas for each ROI
-subplot(3,1,3), hold on
+if ~plotBig, subplot(3,1,3), hold on, else, figure, hold on, end
 for roi = 1:length(roiNames)
     roiBetas = [task{1}.averagedBetas{roi}(:); task{2}.averagedBetas{roi}(:)];
     scatter(repmat(roi,length(roiBetas),1)', roiBetas,'k','filled','MarkerFaceAlpha',.01);
@@ -205,17 +235,20 @@ figure
 for sub = 1:2:length(roiNames);
     % get the individual trials and reduce dimensions
     singleTrials = cat(2, allBetasCombinedFiltered{sub}{[1 6 7 12]});
-    if min(size(singleTrials)) > 1
+    if size(singleTrials,1) > 2
         %[y stress] = mdscale(pdist(singleTrials'),2);
         [y, stress] = tsne(singleTrials');
     
         % plot the individual trials in different colors
         subplot(4,5,ceil(sub/2)), hold on
-        scatter(y(1:40,1),y(1:40,2),'g','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
-        scatter(y(41:80,1),y(41:80,2),'b','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
-        scatter(y(81:120,1),y(81:120,2),'r','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
-        scatter(y(121:160,1),y(121:160,2),'m','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
+        scatter(y(1:numStimRepeats,1),y(1:numStimRepeats,2),'g','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
+        scatter(y((numStimRepeats+1):(numStimRepeats*2),1),y((numStimRepeats+1):(numStimRepeats*2),2),'b','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
+        scatter(y((numStimRepeats*2+1):(numStimRepeats*3),1),y((numStimRepeats*2+1):(numStimRepeats*3),2),'r','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
+        scatter(y((numStimRepeats*3+1):(numStimRepeats*4),1),y((numStimRepeats*3+1):(numStimRepeats*4),2),'m','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
         title(roiNames(sub))
+
+        %save the number of voxels you have
+        numUsableVoxelsByROI(sub) = size(singleTrials,1);
     end
     
     %label
@@ -244,16 +277,25 @@ singleTrialsBigROI = cat(2, allBetasBigROI{[1 6 7 12]});
 
 % plot the individual trials in different colors
 figure, hold on
-scatter(y(1:40,1),y(1:40,2),'g','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
-scatter(y(41:80,1),y(41:80,2),'b','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
-scatter(y(81:120,1),y(81:120,2),'r','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
-scatter(y(121:160,1),y(121:160,2),'m','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
+conf = 0.50;
+% grass
+scatter(y(1:numStimRepeats,1),y(1:numStimRepeats,2),'g','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
+er1 = error_ellipse(cov(y(1:numStimRepeats,:)),[mean(y(1:numStimRepeats,1)) mean(y(1:numStimRepeats,2))],conf); er1.Color = 'g';
+%leaves
+scatter(y((numStimRepeats+1):(numStimRepeats*2),1),y((numStimRepeats+1):(numStimRepeats*2),2),'b','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
+er2 = error_ellipse(cov(y((numStimRepeats+1):(numStimRepeats*2),:)),[mean(y((numStimRepeats+1):(numStimRepeats*2),1)) mean(y((numStimRepeats+1):(numStimRepeats*2),2))],conf); er2.Color = 'b';
+%lemons
+scatter(y((numStimRepeats*2+1):(numStimRepeats*3),1),y((numStimRepeats*2+1):(numStimRepeats*3),2),'r','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
+er3 = error_ellipse(cov(y((numStimRepeats*2+1):(numStimRepeats*3),:)),[mean(y((numStimRepeats*2+1):(numStimRepeats*3),1)) mean(y((numStimRepeats*2+1):(numStimRepeats*3),2))],conf); er3.Color = 'r';
+%bananas
+scatter(y((numStimRepeats*3+1):(numStimRepeats*4),1),y((numStimRepeats*3+1):(numStimRepeats*4),2),'m','filled','MarkerEdgeColor','w','MarkerFaceAlpha',.5)
+er4 = error_ellipse(cov(y((numStimRepeats*3+1):(numStimRepeats*4),:)),[mean(y((numStimRepeats*3+1):(numStimRepeats*4),1)) mean(y((numStimRepeats*3+1):(numStimRepeats*4),2)),conf]); er4.Color = 'm';
 
 %label
 title('Combined Ventral ROI')
 xlabel('Dimension 1')
 ylabel('Dimension 2')
-legend('Grass','Leaves','Lemons','Bananas')
+legend('Grass','','Leaves','','Lemons','','Bananas','')
 
 
 
@@ -262,7 +304,7 @@ endpointIndices = [1 6 7 12];
 colors = {'g','g','g','g','g','g',[0.9290 0.6940 0.1250],[0.9290 0.6940 0.1250],[0.9290 0.6940 0.1250],[0.9290 0.6940 0.1250],[0.9290 0.6940 0.1250],[0.9290 0.6940 0.1250]};
 endpointNames = {'Grass', 'Leaves', 'Lemons', 'Bananas'};
 
-%plot the correlations OF ALL 40 REPEATS with the other 40 repeats from the endpoint conditions
+%plot the correlations OF ALL REPEATS with the other repeats from the endpoint conditions
 if showDistancesSingleTrials
     figure
     for endpoint = 1:length(endpointIndices)
@@ -291,7 +333,7 @@ for endpoint = 1:length(endpointIndices)
         %calculate bootstraps sampling different presentations for the mean
         corBoots = [];
         for boot = 1:numBoots
-            corBoots = [corBoots corr(mean(allBetasBigROI{endpointIndices(endpoint)}(:,randi(40,1,40)),2), mean(allBetasBigROI{interp}(:,randi(40,1,40)),2))];
+            corBoots = [corBoots corr(mean(allBetasBigROI{endpointIndices(endpoint)}(:,randi(numStimRepeats,1,numStimRepeats)),2), mean(allBetasBigROI{interp}(:,randi(numStimRepeats,1,numStimRepeats)),2))];
         end
         errorbar(interp, mean(corBoots), mean(corBoots)-prctile(corBoots,5), prctile(corBoots,95)-mean(corBoots), 'k')
         scatter(interp, mean(corBoots), 'filled','markerFaceColor',colors{interp})
@@ -308,7 +350,7 @@ end
 
 
 
-%% Try doing to mds on the interpolations...
+%% Try doing to multi-dimensional scaling on the interpolations...
 %average the trials together
 for interp = 1:12
     allBetasBigROIAveraged{interp} = mean(allBetasBigROI{interp}, 2);
@@ -334,23 +376,22 @@ if showAvgMDS
 end
 
 
-%% do maximum likelihood distance scaling on the averaged representation
+%% do maximum likelihood distance scaling on the averaged representation with voxels from all ROIs
 interpSets = {[1:6], [7:12]};
-bootstrap = 1;
 figure
 
 disp('Doing mlds - takes a minute or so.')
 %iterate through different interps
-for repititions = 1:25
+for repititions = 1:mldsReps
 
     %average a different set of trials if bootstrapping
-    if bootstrap
+    if mldsReps > 1
         for interp = 1:12
-            allBetasBigROIAveragedMlds{interp} = mean(allBetasBigROI{interp}(:,randi(40,1,40)), 2);
+            allBetasBigROIAveragedMlds{interp} = mean(allBetasBigROI{interp}(:,randi(numStimRepeats,1,numStimRepeats)), 2);
         end
         %if not bootstrapping, use the average of all presentations
     else
-        allBetasBigROIAveragedMlds = allBetasBigROIAveraged;
+            allBetasBigROIAveragedMlds = allBetasBigROIAveraged;
     end
 
     %do the mlds
@@ -366,18 +407,19 @@ for repititions = 1:25
         responses = [];
         for trial = 1:numSamples
             responses(trial) = corMatrix(ims(1,trial), ims(2,trial)) < corMatrix(ims(3,trial), ims(4,trial));
+            j = ims(1,trial); k = ims(2,trial); l = ims(3,trial); m = ims(4,trial);
+            if j == k | l == m | isequal(sort([j k]), sort([l m]));
+                responses(trial) = 2;
+            end
         end
 
         % set up initial params
         psi = [0.5 0.5 0.5 0.5];
-        sigma = 1;
+        sigma = .3;
         initialParams = [psi, sigma];
         
         %options
-        options = optimset('fminsearch');
-        options.MaxFunEvals = 10000;  
-        options.MinFunEvals = 0;  
-        options.MaxIter = 5000;
+        options = optimset('fminsearch'); options.MaxFunEvals = 10000; options.MinFunEvals = 0; options.MaxIter = 5000;
         %options.TolFun = .0001;
         
         %search for params
@@ -391,12 +433,14 @@ for repititions = 1:25
         scatter(1:6,psi, 'filled', 'MarkerFaceColor', colors{max(interpSets{set})})
         gaussFit = fitCumulativeGaussian(1:6, psi);
         PSE = gaussFit.mean;
+        PSEs{set}(repititions) = PSE;
         plot(gaussFit.fitX,gaussFit.fitY,'color',colors{max(interpSets{set})})
         scatter(gaussFit.mean,.5,50,'MarkerFaceColor','r','MarkerEdgeColor','w')
         plot([1 6], [0 1],'k','lineStyle','--')
     
         %limits and label
         ylim([-0.05, 1.05]);
+        xlim([1 6]);
         xlabel('Synthesized interpolation value')
         ylabel('Neural interpolation value')
         if set == 1; title('Grass to leaves mlds'); elseif set == 2, title('Lemons to bananas mlds'), end
@@ -404,10 +448,91 @@ for repititions = 1:25
     end
 end
 
-%%
-
-
 keyboard
+
+%% do maximum likelihood distance scaling on the averaged representation across different ROIs
+figure, sub = 1; numSubs = sum(numUsableVoxelsByROI > 0); allSubs = 1:length(numUsableVoxelsByROI);
+
+disp('Doing mlds on the individual ROIs - takes a bit.')
+%iterate through different ROIs
+for roi = [1 3 5 7]%allSubs(numUsableVoxelsByROI>2)
+
+    %iterate through different interps
+    for repititions = 1:mldsReps
+    
+        %average a different set of trials if bootstrapping
+        if mldsReps > 1
+            for interp = 1:12
+                allBetasSingleROIAveragedMlds{interp} = mean(allBetasCombinedFiltered{roi}{interp}(:,randi(numStimRepeats,1,numStimRepeats)), 2);
+            end
+            %if not bootstrapping, use the average of all presentations
+        else
+            for interp = 1:12
+                allBetasSingleROIAveragedMlds{interp} = mean(allBetasCombinedFiltered{roi}{interp}, 2);
+            end
+        end
+    
+        %do the mlds
+        for set = 1:length(interpSets)
+        
+            %simulate n draws of 4 images
+            numSamples = 10000;
+            averagedInterps = cat(2, allBetasSingleROIAveragedMlds{interpSets{set}});
+            corMatrix = corr(averagedInterps);
+            ims = randi(6,4,numSamples);
+        
+            %calculate which pair has a higher correlation
+            responses = [];
+            for trial = 1:numSamples
+                responses(trial) = corMatrix(ims(1,trial), ims(2,trial)) < corMatrix(ims(3,trial), ims(4,trial));
+                j = ims(1,trial); k = ims(2,trial); l = ims(3,trial); m = ims(4,trial);
+                if j == k | l == m | isequal(sort([j k]), sort([l m]));
+                responses(trial) = 2;
+            end
+            end
+    
+            % set up initial params
+            psi = [0.5 0.5 0.5 0.5];
+            sigma = .3;
+            initialParams = [psi, sigma];
+            
+            %options
+            options = optimset('fminsearch'); options.MaxFunEvals = 10000; options.MinFunEvals = 0; options.MaxIter = 5000;
+            %options.TolFun = .0001;
+            
+            %search for params
+            optimalParams = fminsearch(@(params) computeLoss(params, ims, responses), initialParams, options);
+            psi = [0 optimalParams(1:4) 1];
+            psi = (psi-min(psi));
+            psi = psi/max(psi);
+            
+
+            %plot
+            subplot(2,numSubs,(set-1)*length(allSubs(numUsableVoxelsByROI>2))+sub), hold on
+            scatter(1:6,psi, 'filled', 'MarkerFaceColor', colors{max(interpSets{set})})
+            gaussFit = fitCumulativeGaussian(1:6, psi);
+            PSE = gaussFit.mean;
+            PSEs{set}(repititions) = PSE;
+            plot(gaussFit.fitX,gaussFit.fitY,'color',colors{max(interpSets{set})})
+            scatter(gaussFit.mean,.5,50,'MarkerFaceColor','r','MarkerEdgeColor','w')
+            plot([1 6], [0 1],'k','lineStyle','--')
+        
+            %limits and label
+            ylim([-0.05, 1.05]); xlim([1 6])
+            %xlabel('Synthesized interpolation value')
+            %ylabel('Neural interpolation value')
+            if set == 1; title(roiNames{roi},'Grass to leaves'); elseif set == 2, title(roiNames{roi},'Lemons to bananas'), end
+        
+        end
+    end
+    sub = sub+1;
+end
+
+    
+keyboard
+
+
+
 
 
 
